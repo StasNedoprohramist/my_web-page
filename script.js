797bda9649dae9b0719e7448c49152241d74e704
Stanislav <dulebastanislav210@gmail.com>
2026-01-30 20:48:41 +0200

    Changed way to hide and show main text

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const button = document.querySelector(".btn");
 const mainText = document.querySelector(".main-text");
 
 button.addEventListener("click", () => {
-    if (mainText.innerHTML === "Hello world!") {
-        mainText.innerHTML = "";
+    if (mainText.style.display != "none") {
+        mainText.style.display = "none";
         button.innerHTML = "show text";
     } else {
-        mainText.innerHTML = "Hello world!";
+        mainText.style.display = "inline";
         button.innerHTML = "Hide text";
     }
 });
